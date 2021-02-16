777523d23606e36fc38947cbb5090c68f3553cdc
emily <emily.lawlor@gmail.com>
2021-02-16 13:06:18 +0000

    documentation for stack class

--- src/Stack.java
+++ src/Stack.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Stack {
+    /**
+     *
+     * initialises an empty stack and supplies methods to add, remove elements to the stack, and a method to view the
+     * element at the top of the stack and to view the entire stack
+     *
+     */
     private List<String> stack;
     private int cursor;
 
@@ -10,20 +16,39 @@ public class Stack {
         this.cursor = 0;
     }
 
+    /**
+     *
+     * add a new element to the top of the stack, move the cursor onto this element
+     *
+     * @param element String element that will be added to the top of the stack
+     */
     public void push(String element){
         this.stack.add(element);
         this.cursor += 1;
     }
 
+    /**
+     *
+     * take the element of the top of the stack and return it, move the cursor back to the element that was previously
+     * second on the stack and is no at the top
+     *
+     * @return the element that was at the top of the stack
+     */
     public String pop(){
         this.cursor -= 1;
         return this.stack.remove((this.cursor));
     }
 
+    /**
+     * prints the element at the top of the stack to the console
+     */
     public void top(){
         System.out.println(this.stack.get((this.cursor-1)));
     }
 
+    /**
+     * prints the entire stack, starting from the top working down
+     */
     public void print(){
         int i = this.cursor - 1;
         while (i>=0){
