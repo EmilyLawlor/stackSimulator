688af5833da6f70bf49abe2ee1e041d37e6b334e
emily <emily.lawlor@gmail.com>
2021-02-17 18:24:54 +0000

    JUnit test for show function passing

--- src/TestSimulator.java
+++ src/TestSimulator.java
@@ -1,10 +1,18 @@
 import static org.junit.Assert.*;
+
+import org.junit.After;
 import org.junit.Test;
 
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class TestSimulator{
     private static Simulator stack;
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
     @Test
     public void testingPushFunction(){
@@ -28,6 +36,27 @@ public class TestSimulator{
         stack.push("third");
         String element = stack.pop();
         assertEquals("third", element);
+    }
+
+    @Test
+    public void testingShowFunction(){
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        Simulator stack = new Simulator();
+        stack.push("first");
+        stack.push("second");
+        stack.push("third");
+        String expected = "third" + "\r\n" + "second" + "\r\n" + "first";
+        stack.show();
+
+        assertEquals(expected, outputStreamCaptor.toString()
+                .trim());
+    }
+
+
+    @After
+    public void tearDown() {
+        System.setOut(standardOut);
     }
 
 }
