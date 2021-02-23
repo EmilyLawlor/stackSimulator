7eeedc183179b10808b1e3629cdcc7c5439d6c85
emily <emily.lawlor@gmail.com>
2021-02-23 17:27:59 +0000

    extra comments added to simulator class

--- src/Simulator.java
+++ src/Simulator.java
@@ -9,10 +9,13 @@ public class Simulator {
      */
     public static void main(String[] args) {
 
+        // new, empty stack
         Stack stack = new Stack();
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("push")) {
+                // argument after "push" command will be the element that needs to be added to stack
                 stack.push(args[i + 1]);
+                // skip over element once its been pushed
                 i++;
             } else if (args[i].equals("pop")) {
                 stack.pop();
